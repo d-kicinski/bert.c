198e9be6be30bc660f866fc90d7651190de43c94
Dawid Kicinski <kicinski.dawid@gmail.com>
2024-02-22 21:46:46 +0100

    To drunk to code lol

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@
 
 BUILD_DIR = build
 
+all: build run
+
 run: build
 	./$(BUILD_DIR)/bert
 	
 build:
+	mkdir -p $(BUILD_DIR)
 	gcc bert.c -o $(BUILD_DIR)/bert -DBERT_IMPLEMENTATION
 
--- bert.c
+++ bert.c
@@ -1,3 +1,12 @@
+/*
+ * Rules:
+ *  - plain C
+ *  - no dynamic memory allocation allowed
+ *  - only inference pass
+ *  - as simple and straightforward as possible
+ *  - the code must be written while consuming at least 660ml of beer.
+ *  - AI guided coding is allowed but has to be limited to its minium
+ */
 #include <stdio.h>
 
 #define SHAPE_0 128
@@ -6,12 +15,30 @@ typedef struct {
 
 } shape_2d;
 
-void mm(float lhs[SHAPE_0], float rhs[SHAPE_0], float out[SHAPE_0]) {
+/*
+ * This comuptes the matrix multiplicaton as follows
+ *    Y = A @ B
+ * where the shapes are exepect as
+ *    A[X, Y]
+ *    B[Z, Y]
+ *    Y[X, Z]
+ */
+void mm(float A[SHAPE_0], float B[SHAPE_0], float C[SHAPE_0]) {
   // do some wild computations
 }
 
 #ifdef BERT_IMPLEMENTATION
 
-int main(int argc, char **argv) { printf("Hello world!\n"); }
+int main(int argc, char **argv) {
+
+  // Feed forward network
+  // Y = X @ W
+  //
+  // X: [B, A, B];
+  // W: [B, B, C];
+  // Y: [B, A, C];
+
+  printf("Hello world!\n");
+}
 
 #endif
