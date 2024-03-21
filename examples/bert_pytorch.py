18cbf76a18f1f4af6688a661f966bf56c6b64322
Dawid Kicinski <kicinski.dawid@gmail.com>
2024-03-21 22:21:02 +0100

    Add python sample code for reminding how bert works

---
+++ examples/bert_pytorch.py
@@ -0,0 +1,17 @@
+from transformers import BertModel, BertTokenizer
+
+
+def show_bert():
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    model = BertModel.from_pretrained(model_name)
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    text = "hello there"
+    model_input = tokenizer(text, return_tensors="pt")
+    print(model_input)
+
+    embeddings = model(input_ids=model_input["input_ids"]).pooler_output
+    print(embeddings.shape)
+
+
+if __name__ == '__main__':
+    show_bert()
